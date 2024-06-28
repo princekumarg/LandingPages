4b87037ca8b3a33cc74d19ed32501835d137e628
prince kumar <agarwal8789@gmail.com>
2024-06-28 11:48:04 +0530

    commit made on 404 pages

--- responsive404page/assets/js/main.js
+++ responsive404page/assets/js/main.js
@@ -27,6 +27,20 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
+}
+window.addEventListener('scroll', shadowHeader);
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__img-1', { duration: 2, y: -100 });
+let tl1 = gsap.timeline();
+tl1.from('.home__img-2', { duration: 1, x: -400, y: -50, rotation: 32, scale: .5 })
+    .to('.home__img-2', { duration: 1, rotation: 15, scale: 1.2 })
+    .to('.home__img-2', { duration: 1, rotation: 0, scale: 1 });
+let tl2 = gsap.timeline();
+tl2.from('.home__img-3', { duration: 1, x: 50, y: -50, rotation: 30 })
+    .to('.home__img-3', { duration: 5, rotation: 360 });
+gsap.from('.home__img-4', { duration: 5, y: -100, scale: 1.3 });
+gsap.from('.home__img-5', { duration: 3, y: 100 });
